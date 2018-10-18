a26826ec8f86d11dc5e9f7320f6b8e374fee4165
Lukas Starka <l.starka@students.htl-leonding.ac.at>
2018-10-18 18:45:08 +0200

    Kann benotet werden

--- src/main/java/at/htl/robot/gui/Main.java
+++ src/main/java/at/htl/robot/gui/Main.java
@@ -218,7 +218,7 @@ public class Main extends PApplet {
                 omicron.changeMode();
                 theta.changeMode();
             } else if ((key == control) || (key == control2)) {
-                omicron.changeConytrol();
+                omicron.changeControl();
                 theta.changeControl();
             }
             else if (key == stepForwardT) {
